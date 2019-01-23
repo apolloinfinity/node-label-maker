af7d44c2d7e8879c225af6333221966940825311
Javier Ramirez <apollo.infinity88@gmail.com>
2019-01-23 10:02:17 -0800

    Renamed file to better reflect what it does and practicing try/catch in async/await

---
+++ controllers/labelsController.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const fs = require("fs");
+
+const pdfDocument = require("pdfkit");
+const moment = require("moment");
+
+const rootDir = require("../utils/path");
+
+exports.printLabels = async (req, res) => {
+
+    try {
+        res.send('<h1>HEllo</h1>');
+
+
+    } catch (err) {
+        console.error(err);
+    }
+
+}
