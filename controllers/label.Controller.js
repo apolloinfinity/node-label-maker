ee712dd52cec41088a984ebd4303a127667bf14b
Javier Ramirez <apollo.infinity88@gmail.com>
2019-06-05 14:45:01 -0700

    Finished the post route for the label maker. Need to add more to get request and include a DB

--- controllers/label.Controller.js
+++ controllers/label.Controller.js
@@ -11,13 +11,45 @@ exports.getLabels = async (req, res) => {
 }
 
 exports.createLabel = async (req, res) => {
+    // Destructured body 
+    const { number, description, lot, quantity, warehouse, received, dataURL } = await req.body;
+    // console.log(dataURL)
+    const doc = new pdfDocument({
+        layout: "landscape",
+        size: [200, 450],
+        margin: 10
+    });
 
-    try {
-        res.send('<h1>HEllo</h1>');
+    const labelName = new Date().toISOString().replace(/:/g, "-");
+    const destination = "test";
+    const printDate = moment().format("L");
 
+    function itemLabel(itemNum, description, labelName, lot, loc, qr) {
 
-    } catch (err) {
-        console.error(err);
-    };
+        const imgLoc = path.join(rootDir, "assets", "bolts", "hex-bolt.jpg");
+        const jobName = `${itemNum}-${labelName}.pdf`;
+        const inventoryPath = path.join(rootDir, "test", jobName);
+
+        doc.pipe(fs.createWriteStream(inventoryPath));
+        doc.pipe(res);
+        doc.image(qr, 10, 10, {
+            fit: [150, 150],
+            align: "center",
+            valign: "top",
+        });
+        doc.image(imgLoc, 200, 10, {
+            fit: [190, 180],
+            align: "right",
+            valign: "top"
+        })
+        doc.font("Helvetica")
+            .fontSize(18)
+            .text(`${itemNum} - ${lot}`, 190, 100);
+        doc.text(description, 190, 120);
+        doc.text(`Location: ${loc}`, 190, 140);
+        doc.end()
+    }
+
+    itemLabel(number, description, labelName, lot, warehouse, dataURL)
 
 }
